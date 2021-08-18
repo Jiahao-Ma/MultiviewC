343e871d7630621d282b5bb2e051b8fdb5f21caa
Marko <1152616455@qq.com>
2021-08-18 10:49:41 +0800

    Update `visible` attributes to the dataset

--- data.py
+++ data.py
@@ -4,7 +4,8 @@ from PIL import Image
 import numpy as np
 from utils.utils import draw_3DBBox, vis_colors, vis_styles, compute_3d_bbox, corners8_to_rect4
 class MultiviewCow(object):
-    def __init__(self, json_root=r'annotations', 
+    def __init__(self, root = r'.',
+                       json_root=r'annotations', 
                        img_root =r'images', 
                        calib_root=r'calibrations', 
                        cam_range=range(1, 8),
@@ -16,9 +17,9 @@ class MultiviewCow(object):
             calib_root: calibration path
             cam_range: default value： range(1, 8), represent the camera ID
         """
-        self.json_root = json_root
-        self.img_root = img_root
-        self.calib_root = calib_root
+        self.json_root = os.path.join(root, json_root)
+        self.img_root = os.path.join(root, img_root)
+        self.calib_root = os.path.join(root, calib_root)
         self.cam_range = cam_range
 
     def __len__(self):
@@ -75,7 +76,7 @@ class MultiviewCow(object):
         assert camid in range(0, 7), "camera index ranges from 0 to 6"
         
         annotations, image_fnames, calib_fnames = self.__getitem__(index)
-        annotation = annotations['C{}'.format(1)]
+        annotation = annotations['C{}'.format(camid+1)]
         image_fname = image_fnames[camid]
         calib_fname = calib_fnames[camid]
         #--------------------------------#
@@ -99,17 +100,22 @@ class MultiviewCow(object):
         ax = fig.add_subplot(111)
         ax.imshow(image)
         ax.axis('off')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
         for ann in annotation:
             # read annotation and compute 3D bbox in 2D image
+            visible = ann['visible']
+            if not visible:
+                continue 
             label = ann['CowID']
             action = ann['action']
             location = ann['location']
             rotation = ann['rotation']
             dimension = ann['dimension']
             corner_2d = compute_3d_bbox(dimension, rotation, location, project_mat)
-            mask = np.logical_and(corner_2d[:, 0] > 0 , corner_2d[:, 0] < 1280) & np.logical_and(corner_2d[:, 1] > 0 , corner_2d[:, 1] < 720)[0]
+            # mask = np.logical_and(corner_2d[:, 0] > 0 , corner_2d[:, 0] < 1280) & np.logical_and(corner_2d[:, 1] > 0 , corner_2d[:, 1] < 720)[0]
             
-            corner_2d = corner_2d[mask]
+            # corner_2d = corner_2d[mask]
             if len(corner_2d) != 8:
                 continue
             [xmin, ymin, xmax, ymax] = corners8_to_rect4(corner_2d)
@@ -124,13 +130,14 @@ class MultiviewCow(object):
                 rect = plt.Rectangle([xmin, ymin], width, height, color=(1, 0, 0), linewidth=3, fill=False)
                 ax.add_patch(rect)
             ax = draw_3DBBox(ax, corner_2d, linewidth=linewidth3D)
-            ax.text(xmin, ymin-5, s='{}: {}'.format(label, action), **styples)
+            ax.text(corner_2d[4][0], corner_2d[4][1]-15, s='{}: {}'.format(label, action), **styples)
         plt.show()
         return ax
 
 
 if __name__ == '__main__':
+    import sys
     dataset = MultiviewCow()
     annotations, image_fnames, calib_fnames = dataset[0]
-    for i in range(7):
+    for i in range(0,7):
         dataset.visualize(index=0, camid=i, show_2D_bbox=False)
